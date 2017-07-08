88407e1e22a9cc3c90788ba245e007fbaf8a3237
SomethingStrString <eerikasykes@outlook.com>
2017-07-08 20:55:33 +0100

    Removed earlier additions, and simplified logic

--- PassGen.py
+++ PassGen.py
@@ -8,37 +8,21 @@ import sys
 def userInput() :
     """Asks user what length of password to create."""
 
-    userLength=1
+    userLength=0
 
     try : 
         userLength = int(input('How many characters would you like in your password? '))
     except : 
             print('\nSorry there was an error')
-            userErrorChoice()
-    
+            sys.exit()
+                       
     return(userLength)
-
-
-def userErrorChoice() :
-    """Asks user if they want to try again or exit."""
-    
-    userChoice = input('Type (A) to try again or (X) to quit:' ).upper()
-
-    if userChoice == 'A' :
-        generateRandom()
-
-    elif userChoice == 'X' :
-        sys.exit()
-
-    else :
-        clearConsole(0)
-        userErrorChoice() #repeats question until user answers A or X
 
 
 def generateRandom() : 
     """Generate random characters to form a password."""
 
-    charsets = [
+    charSets = [
         'abcdefghijklmnopqrstuvwxyz',
         'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
         '0123456789',
@@ -47,10 +31,10 @@ def generateRandom() :
 
     length = userInput()
     pwd = []
-    charset = choice(charsets)
+    charset = choice(charSets)
     while len(pwd) < length:
         pwd.append(choice(charset))
-        charset = choice(list(set(charsets) - set([charset])))
+        charset = choice(list(set(charSets) - set([charset])))
     return outputPassword("".join(pwd))
 
 
@@ -60,25 +44,6 @@ def outputPassword(passwordString) :
     print('\nThank you for using PassGen')
     print('This is your password: {0}\n'.format(passwordString))
 
-
-def clearConsole(wait) : #function to clear console on Linux or Windows
-    """Clears console, with optional time delay.
-
-    Will attempt to clear the console for Windows, should that fail it will attempt to clear the
-    console for Linux.
-    """
-
-    import time
-    time.sleep(wait) 
-    # produces a delay based on the argument given to clearConsole()
-    
-    import os
-
-    try :
-       os.system('cls') #clears console on Windows
-
-    except :
-       os.system('clear') #clears console on Linux
 
 def main() : 
     
