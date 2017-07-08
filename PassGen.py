1483281e09482a6929532878b30f68c2525c5b53
SomethingStrString <eerikasykes@outlook.com>
2017-07-09 00:01:44 +0100

    more work - in progress o splitOutput()

--- PassGen.py
+++ PassGen.py
@@ -61,7 +61,15 @@ def splitOutput(passwordString) :
 
     #for pos in range(0, size, chunkSize):
     #    yield passwordString[pos:pos+chunkSize]
+    upper = 10
+    lower = 0
 
+    for steps in range(len(passwordString)) :
+        string = passwordString[lower:upper]
+        print(string)
+        upper = upper + upper
+        lower = lower + 10
+        
 
 def outputPassword(passwordString) :
     """Output password to user."""
@@ -70,6 +78,8 @@ def outputPassword(passwordString) :
     print('This is your password: {0}\n'.format(passwordString))
 
     # Trying to implement a new line to the string after every 10 characters
+
+    splitOutput(passwordString)
     
 
 def main() : 
