925d01e841303950dd2dadbf5ab3e23957239ff2
SomethingStrString <eerikasykes@outlook.com>
2017-07-08 22:29:26 +0100

    Drafting method to split the string into 10 character chunks

--- PassGen.py
+++ PassGen.py
@@ -50,12 +50,27 @@ def generateRandom() :
     return outputPassword("".join(pwd))
 
 
+def splitOutput(passwordString) :
+    """Split output into seperate lines of 10 characters."""
+
+    # We assume here that x is an int and > 0
+
+    #x = 10
+    #size = len(passwordString)
+    #chunkSize = size//x # // truncates a float to avoid file type error
+
+    #for pos in range(0, size, chunkSize):
+    #    yield passwordString[pos:pos+chunkSize]
+
+
 def outputPassword(passwordString) :
     """Output password to user."""
 
     print('\nThank you for using PassGen')
     print('This is your password: {0}\n'.format(passwordString))
 
+    # Trying to implement a new line to the string after every 10 characters
+    
 
 def main() : 
     
