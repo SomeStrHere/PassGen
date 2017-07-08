b5efbd1bccea972452e97fa1be4efef91f7c7d56
SomethingStrString <eerikasykes@outlook.com>
2017-07-08 20:31:45 +0100

    Added clearConsole() for in case of error

--- PassGen.py
+++ PassGen.py
@@ -31,6 +31,7 @@ def userErrorChoice() :
         sys.exit()
 
     else :
+        clearConsole(0)
         userErrorChoice() #repeats question until user answers A or X
 
 
@@ -58,6 +59,26 @@ def outputPassword(passwordString) :
 
     print('\nThank you for using PassGen')
     print('This is your password: {0}\n'.format(passwordString))
+
+
+def clearConsole(wait) : #function to clear console on Linux or Windows
+    """Clears console, with optional time delay.
+
+    Will attempt to clear the console for Windows, should that fail it will attempt to clear the
+    console for Linux.
+    """
+
+    import time
+    time.sleep(wait) 
+    # produces a delay based on the argument given to clearConsole()
+    
+    import os
+
+    try :
+       os.system('cls') #clears console on Windows
+
+    except :
+       os.system('clear') #clears console on Linux
 
 def main() : 
     
